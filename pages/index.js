80e38e6f784da9ecada5ed692cf3c1adb1e64abd
teodorazhelyazkova <teodora.s.zhelyazkova@gmail.com>
2022-10-05 12:18:28 +0300

    add Main component to app page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
-import { Hero } from "../sections";
+import { Hero } from "../sections/Hero/";
+import { Main } from "../sections/Main/";
 
 const heroProps = {
   image: { src: "/img/heroimg.png", alt: "", width: 1000, height: 1680 },
@@ -7,11 +8,17 @@ const heroProps = {
   ctaText: "Start",
 };
 
+const mainProps = {
+  image: { src: "/../resources/video.png", alt: "", width: 1000, height: 1680 },
+  title: "Managed agency selection",
+  description: "Strengthen your onboarding process"
+};
+
 export default function Home() {
   return (
     <>
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
@@ -20,7 +27,7 @@ export default function Home() {
       </head>
       <div>
         <Hero {...heroProps} />
-        {/** Other sections */}
+        <Main {...mainProps} />
       </div>
     </>
   );
