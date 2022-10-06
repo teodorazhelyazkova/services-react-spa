ed7992507770bdd46ba9e9f1dd45177eaf35f872
teodorazhelyazkova <teodora.s.zhelyazkova@gmail.com>
2022-10-06 20:54:46 +0300

    add changes to styles of the SectionContainer component

--- components/Containers/SectionContainer/elements.jsx
+++ components/Containers/SectionContainer/elements.jsx
@@ -2,9 +2,6 @@ import styled from "styled-components";
 
 export const StyledSectionContainer = styled(({ topMargin = 0, bottomMargin = 0, ...props }) => <div {...props} />)`
   display: flex;
-  max-width: 1920px;
-  width: 100%;
-  height: auto;
   margin-left: auto;
   margin-right: auto;
   margin-top: ${({ topMargin }) => topMargin}rem;
