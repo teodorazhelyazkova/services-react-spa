e974d3776c9af675a0aa1288408e841644ee8282
teodorazhelyazkova <teodora.s.zhelyazkova@gmail.com>
2022-10-06 20:52:27 +0300

    add styles for Main component

--- sections/Main/Main.jsx
+++ sections/Main/Main.jsx
@@ -1,44 +1,64 @@
 import Image from "next/image";
-import { Cards } from "../../collections/Cards";
+import { Cards } from "~/collections/Cards";
+
+import {
+  StyledImageContainer,
+  StyledTextContainer,
+  StyledTitle,
+  StyledDescription,
+  StyledServicesContainer,
+  StyledContainer,
+} from "./elements";
 
 const cardsData = [
   {
-    image: { src: "/img/sampleIcon.svg", alt: "", width: 30, height: 30 },
+    image: { src: "/img/sampleIcon.svg", alt: "", width: 50, height: 50 },
     title: "Brief",
-    description:
-      "Complete brief writing or simple guidance on what to include, we've got you covered.",
+    description: [
+      "Complete ",
+      <strong key="">brief writing or simple guidance</strong>,
+      " on what to include, we've got you covered.",
+    ],
   },
   {
-    image: { src: "/img/sampleIcon.svg", alt: "", width: 30, height: 30 },
+    image: { src: "/img/sampleIcon.svg", alt: "", width: 50, height: 50 },
     title: "Search",
-    description:
-      "In-depth agency search covering: criteria matching, door knocking and due-dilligence vetting.",
+    description: [
+      "In-depth agency search covering: ",
+      <strong key="">criteria matching</strong>,
+      " door knocking and due-dilligence vetting.",
+    ],
   },
   {
-    image: { src: "/img/sampleIcon.svg", alt: "", width: 30, height: 30 },
+    image: { src: "/img/sampleIcon.svg", alt: "", width: 50, height: 50 },
     title: "Pitch",
-    description:
-      "Comprehensive pitch management, including comms, diary management and pitch hosting.",
+    description: [
+      "Comprehensive ",
+      <strong key="">pitch management</strong>,
+      " including comms, diary management and pitch hosting.",
+    ],
   },
 ];
 
 export const Main = ({ image, title, description }) => {
   return (
-    <div>
-      <div>
-        <Image
-          layout="responsive"
-          src={image.src}
-          alt={image.alt}
-          width={image.width}
-          height={image.height}
-        />
-      </div>
-      <div>
-        <h2>{title}</h2>
-        <p>{description}</p>
-      </div>
-      <Cards cardsData={cardsData} />
-    </div>
+    <StyledContainer>
+      <StyledTextContainer>
+        <StyledTitle>{title}</StyledTitle>
+        <StyledDescription>{description}</StyledDescription>
+      </StyledTextContainer>
+      <StyledServicesContainer>
+        <StyledImageContainer>
+          <Image
+            layout="responsive"
+            src={image.src}
+            alt={image.alt}
+            width={image.width}
+            height={image.height}
+          />
+        </StyledImageContainer>
+        <Cards cardsData={cardsData} />
+      </StyledServicesContainer>
+    </StyledContainer>
   );
 };
